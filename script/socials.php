2e4d3e7fc005949b59e2de03cd0cfdb9df72ac74
Damien Mauchamp <damienmauchamp@gmail.com>
2019-10-31 14:24:24 +0100

    Social media script to retrieve twitter/instagram ids

--- script/socials.php
+++ script/socials.php
@@ -1,79 +1,110 @@
 <?php
-//
-//
-//
+
 include dirname(__DIR__) . '/config.php';
+header("Content-type:application/json");
 
-$default = array(
-	'id' => null,
-	'name' => "ARTIST",
-	'genius' => array(
-		'id' => "ID_GENIUS",
-		'artistName' => "NAME_GENIUS"
-	),
-	'itunes' => array(
-		'id' => "ID_ITUNES",
-		'artistName' => "NAME_ITUNES"
-	),
-	'instagram' => array(
-		'id' => "ID_INSTAGRAM",
-		'username' => "NAME_INSTAGRAM"
-	),
-	'twitter' => array(
-		'id' => "ID_TWITTER",
-		'username' => "NAME_TWITTER"
-	),
-	'band' => false,
-	'updates' => array(
-		'auto' => new DateTime(),
-		'manually' => false
-	)
-);
+use \InstagramAPI\Instagram;
+use Abraham\TwitterOAuth\TwitterOAuth;
 
-// today's artists
-$todays_file = DIR_DATA . PREFIX_ALBUM_FILE . date("Ymd") . ".json";
-$today = is_file($todays_file) ? json_decode(file_get_contents($todays_file), true) : false;
+$json = file_get_contents(DIR_DATA . "socials.json");
+$data = json_decode($json, true);
 
-// data
-$data = is_file(DIR_DATA . "socials.json") ? json_decode(file_get_contents(DIR_DATA . "socials.json"), true) : array();
+// instagram logging
+$debug = false;
+$ig = new Instagram($debug);
+try { // connexion
+	$ig->login($_ENV["INSTAGRAM_USERNAME"], $_ENV["INSTAGRAM_PASSWD"]);
+} catch (\Exception $e) {
+	echo 'Impossible de se connecter à Instagram: ' . $e->getMessage() . "\n";
+	exit(0);
+}
 
-$newData = $data;
+// twitter logging
+$twit = new TwitterOAuth($_ENV["TWITTER_API_KEY"], $_ENV["TWITTER_API_SECRET_KEY"], $_ENV["TWITTER_ACCESS_TOKEN"], $_ENV["TWITTER_ACCESS_TOKEN_SECRET"]);
+$twit->setTimeouts(60, 30);
 
-//echo json_encode(find_artist($data, 'Passi'));
+// loop
+foreach ($data as $i => $artist) {
 
-// artists names
-foreach ($today['today'] as $year => $items) {
-	foreach ($items as $release) {
-		$artist = find_artist($data, $release['artist']) ?: false;
-		if (!$artist) {
-			$artist = $default;
-			$artist['name'] = $release['artist'];
-			$newData[] = $artist;
-		}
-		//print_r($artist);
+	// instagram
+	$data[$i]['instagram'] = instagram_user($artist['instagram']);
+
+	// instagram
+	$data[$i]['twitter'] = twitter_user($artist['twitter']);
+}
+
+// functions
+function instagram_user($data = null) {
+	global $ig;
+
+	//
+	if ($data === null || $data['username'] === null) {
+		return null;
 	}
-}
 
-echo json_encode($newData);
-// writeJSONFile("socials", $data);
-//echo json_encode($today);
-function equals($a, $b) {
-	return $a === $b;
-}
+	//
+	try {
+		$id = $ig->people->getUserIdForName($data['username']);
+		echo "\n[INSTAGRAM] ID found for username '".$data['username']."' : $id";
+	} catch (\Exception $e) {
+		$id = null;
+		echo "\n[INSTAGRAM] No ID found for username '".$data['username']."'";
+	}
 
-function find_artist($data, $search) {
-	foreach ($data as $id => $artist) {
-		// name
-		if (equals($artist['name'], $search)) {
-			return array('artist' => $artist, 'with' => 'name');
-		} else if (equals($artist['genius']['artistName'], $search)) {
-			return array('artist' => $artist, 'with' => 'genius');
-		} else if (equals($artist['itunes']['artistName'], $search)) {
-			return array('artist' => $artist, 'with' => 'itunes');
-		} else {
-			continue;
+	if ($id === null) {
+		// user does not exist
+		return null;
+	}
+
+	return array(
+		'id' => intval($id),
+		'username' => $data['username']
+	);
+}
+function twitter_user($data = null) {
+	global $twit;
+
+	//
+	if ($data === null || $data['username'] === null) {
+		return null;
+	}
+
+	//
+	try {
+		$user_data = $twit->get('users/lookup', array('screen_name' => $data['username']));
+		echo "\n[Twitter] Results found for term '".$data['username']."'";
+	} catch (\Exception $e) {
+		$user_data = null;
+		echo "\n[Twitter] No results found for term '".$data['username']."'";
+	}
+
+	if ($user_data === null) {
+		// user does not exist
+		return null;
+	}
+
+	//
+	$id = null;
+	foreach ($user_data as $i => $user) {
+		//print_r($user);
+		if (strtolower($user->screen_name) === strtolower($data['username'])) {
+			$id = $user->id;
+			echo "\n[Twitter] ID found for username '".$data['username']."' : $id";
+			break;
 		}
 	}
-	return false;
+
+	if ($id === null) {
+		echo "\n[Twitter] No ID found for username '".$data['username']."'";
+		return null;
+	}
+
+	return array(
+		'id' => intval($id),
+		'username' => $data['username']
+	);
 }
-exit;
+
+
+writeJSONFile("socials", $data);
+//exit(json_encode($data));
