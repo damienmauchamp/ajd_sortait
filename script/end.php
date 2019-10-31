a1d51e7a51d0ff6bc87dbcfd08f09f2dd0e5548f
Damien Mauchamp <damienmauchamp@gmail.com>
2019-10-31 15:10:01 +0100

    end script

--- script/end.php
+++ script/end.php
@@ -3,7 +3,15 @@
 // cron : 30 19 * * *	php script_post.php
 include dirname(__DIR__) . '/config.php';
 
+// logs
+if (!empty($_ENV['LOG_DIR']) && !empty($_ENV['LOG_POST'])) {
+	if (is_file($_ENV['LOG_DIR'] . $_ENV['LOG_POST'] . '.log')) {
+		rename($_ENV['LOG_DIR'] . $_ENV['LOG_POST'] . '.log', $_ENV['LOG_DIR'] . $_ENV['LOG_POST'] . '_'.date('Ymd') . '.log')
+	}
+}
+
 $file = dirname(__DIR__) . "/data/" . $file_prefixe . date("Ymd") . ".json";
 clearImgs();
-if (is_file($file))
+if (is_file($file)) {
 	unlink($file);
+}
